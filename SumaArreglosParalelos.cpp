9067e7dfd5eb76398a3d0f50e45f9e8850cf65ce
A01021190Ian <113574724+A01021190Ian@users.noreply.github.com>
2024-01-27 18:53:59 -0600

    Add files via upload

---
+++ SumaArreglosParalelos.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <time.h>
+#include <iostream>
+#include <ctime>   
+#include <cstdlib>
+using namespace std;
+
+int main()
+{   
+    double itime, ftime, exec_time;
+    int size = 1000;
+    int a[size];
+    int b[size];
+    int c[size];
+    int d[size];
+    cout<<"\nArray A:\n";
+    for(int j=0; j<size; ++j)
+    {
+        a[j]=rand();
+        cout<<a[j]<<" ";
+    }
+    cout<<"\nArray B:\n";
+    for(int k=0; k<size; ++k)
+    {
+        b[k]=rand();
+        cout<<b[k]<<" ";
+    }
+    itime = omp_get_wtime();
+    #pragma omp parallel for
+    for(int i=0; i<size; ++i)
+    {
+        c[i]=a[i]+b[i];
+    }
+    ftime = omp_get_wtime();
+    exec_time = ftime - itime;
+    cout<<"\nArray C:\n";
+    for(int m=0; m<size; ++m)
+    {
+       cout<<c[m]<<" ";
+    } 
+    cout<<"\nWith parallel processing time is: "<< exec_time;
+}
